33b043dcaeda30aeb30ee7ef6335c09c77455057
Lisa <yurirenjyo@gmail.com>
2020-09-01 22:36:38 -0500

    Redirection after Logging in

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@angular/core';
-import {CanActivate, Router} from '@angular/router';
-import {AuthService} from './auth.service';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
- 
-@Injectable({
-  providedIn: 'root'
-})
 
-export class AuthGuardService implements CanActivate{
- 
-  constructor(private auth: AuthService, private router: Router) { }
- 
-   canActivate(): Observable<boolean> {
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardService implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(route, state: RouterStateSnapshot): Observable<boolean> {
     return this.auth.user$.pipe(
       map((user) => {
         if (user) {
           return true;
         } else {
-          this.router.navigate(['/login']);
+          this.router.navigate(['/login'], { queryParams: {returnUrl: state.url }});
           return false;
         }
       })
     );
   }
- 
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,14 @@ import * as firebase from 'firebase/app';
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = afAuth.authState;
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+
     this.afAuth.auth.signInWithRedirect( new firebase.auth.GoogleAuthProvider());
   }
   
